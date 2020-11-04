32d62e86e3a2c846b2fcee2d2e68c91583eff87d
Luis Chilo <gfxargentina@gmail.com>
2020-11-04 11:46:30 -0300

    rename folder for git pages

---
+++ docs/precache-manifest.621053e52bab22300d925debbed1d0ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d3914994cc4b10cab8848048a60df3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "455abbef8cd791976bcc",
+    "url": "/static/css/main.bfcbdda0.chunk.css"
+  },
+  {
+    "revision": "50c5007e2135538c1738",
+    "url": "/static/js/2.a7fcd019.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7fcd019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455abbef8cd791976bcc",
+    "url": "/static/js/main.f4aac531.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
